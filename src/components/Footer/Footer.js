cf860332db1b44ac5bfb73c89a6007cf290af23f
Divit Karnawat <divitkarnawat1@gmail.com>
2020-05-18 18:27:55 +0530

    added footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Footer.css';
+import './Footer.scss';
 import LogoSVG from '../../assets/img/Sansiel_Logo_SVG.svg';
 class Footer extends Component
 {
@@ -17,14 +17,14 @@ class Footer extends Component
 
 
                     </div>
-                    <div className="col-sm-4 col-12 ">
+                    <div className="col-sm-4 col-12 cd">
                         <div className="contact_details">
-                            <div>Musterstrasse, Musterland</div>
-                            <div>+880 168 108 109 1425<br/>+0216 809 172</div>
-                            <div>info@sansiel.com</div>
+        <div><i class="fas fa-map-marker-alt"></i>Musterstrasse, Musterland</div>
+                            <div><i class="fas fa-phone-alt"></i>+880 168 108 109 1425<br/>+0216 809 172</div>
+                            <div><i class="fas fa-envelope"></i>info@sansiel.com</div>
                         </div>
                     </div>
-                    <div className="col-sm-4 col-12 ">
+                    <div className="col-sm-4 col-12 cf">
                         <div className="flinks">
                         <div><a>Datenschutzerklärung</a></div>
                         <div><a>Impressum</a></div>
