57bfbf2a33744b3bea10412d60e9ae63a93e35da
Divit Karnawat <divitkarnawat1@gmail.com>
2020-05-26 13:34:00 +0530

    contact form modifications

--- src/views/components/sections/ContactSection/ContactSection.js
+++ src/views/components/sections/ContactSection/ContactSection.js
@@ -3,6 +3,17 @@ import './ContactSection.css';
 
 class Contact extends Component{
 
+  handleInvalid = (e)=>{
+    e.target.setCustomValidity('Lütfen işaretli yerleri doldurunuz'); 
+  }
+  handleChange = (e)=>{
+    if(e.target.value!="")
+    e.target.setCustomValidity('');
+  }
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    alert('Wir freuen uns, dass Sie sich die Zeit genommen haben, uns zu schreiben.Wir werden uns bald bei Ihnen melden.');
+  }
 render()
 {
     return(
@@ -19,22 +30,22 @@ render()
 <div className="row contactWrapper" >
   
   <div className="col-12">
-  <form  className="wpcf7" action="#">
+  <form  className="wpcf7" onSubmit={this.handleSubmit}>
     <div className = "row">
     <div className="col-12" id= "firstinput">
-      <input required  type="text" id="fname" name="firstname" placeholder="Vorname" />
+      <input required  type="text" id="fname" name="firstname" placeholder="Vorname" onInvalid={this.handleInvalid} onChange={this.handleChange} title="Vorname"/>
     </div>
     <div className="col-12">
-      <input required  type="text" id="lname" name="lastname" placeholder="Nachname" />
+      <input required  type="text" id="lname" name="lastname" placeholder="Nachname" title="Nachname" onInvalid={this.handleInvalid} onChange={this.handleChange}/>
     </div>
     <div className="col-12 " id = "lastinput">
-      <input required  type="text" id="email" name="email" placeholder="E-Mail" />
+      <input required  type="text" id="email" name="email" placeholder="E-Mail" title="E-Mail" onInvalid={this.handleInvalid} onChange={this.handleChange} />
     </div>
      </div>
     
     <div className="row">
         <div className="col-12">
-    <textarea  id="message" name="message" placeholder="Nachricht" rows="8" ></textarea>
+    <textarea  id="message" name="message" placeholder="Nachricht" rows="8" required onInvalid={this.handleInvalid} onChange={this.handleChange}/>
     </div>
     </div>
     <input type="submit" value="Senden" />
