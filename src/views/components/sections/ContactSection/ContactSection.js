c2cb6b8928cb1b48b015f9893be436996c6be02a
Divit Karnawat <divitkarnawat1@gmail.com>
2020-06-04 17:38:10 +0530

    testing

--- src/views/components/sections/ContactSection/ContactSection.js
+++ src/views/components/sections/ContactSection/ContactSection.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './ContactSection.css';
 import emailjs from 'emailjs-com';
-import ReCAPTCHA from 'react-google-recaptcha';
+const axios = require('axios');
+//import ReCAPTCHA from 'react-google-recaptcha';
 class Contact extends Component{
   constructor(props)
   {
     super(props);
     this.state={
-      firstname:"",
-      lastname: "",
+      first_name:"",
+      last_name: "",
       email: "",
       message:"",
       status: ""
@@ -41,8 +42,8 @@ class Contact extends Component{
   
   resetForm = () => {
     this.setState({
-      firstname:"",
-      lastname: "",
+      first_name:"",
+      last_name: "",
       email: "",
       message:"",
       status: ""
@@ -51,34 +52,53 @@ class Contact extends Component{
  
 
   handleSubmit = (e)=>{
-    e.preventDefault();
-    if(this.recaptchaRef.current.getValue())
-    {
-    this.send_status.current.style.color = "orange";
-    this.setState({status: "Senden..."});
-    const {firstname, lastname, email, message} =  this.state;
-    let templateParams = {
-      from_name: firstname,
-      to_name: 'sansiel',
-      reply_to : email,
-      message_html: message,
-      firstname,
-      lastname,
-      email,
-      message
- }
- emailjs.send("gmail", "template_74tsu5x6",templateParams, "user_sWVyIEywbQTDzFH6OGZ98").then((res)=>{
-  this.send_status.current.style.color = "green";
-   this.setState({status: 'Ihre Nachricht wurde gesendet'})
-   setTimeout(()=>{this.resetForm()}, 2000);
- }).catch();
+   e.preventDefault();
+    let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+      }
+    };
+    const {first_name, last_name, email, message} =  this.state;
+    const body = { first_name, last_name, email, message };
+    
+    axios.post('https://sansiel.de/hello.php', {
+      first_name: 'Fred',
+      last_name: 'Flintstone'
+    })
+    .then(function (response) {
+      console.log("form submitted");
+    })
+    .catch(function (error) {
+      console.log("error DKJCBhjbcnxzcnbsjhbkaslknakcbmx nabjc");
+    });
+    // if(this.recaptchaRef.current.getValue())
+    // {
+    // this.send_status.current.style.color = "orange";
+    // this.setState({status: "Senden..."});
+    // const {firstname, lastname, email, message} =  this.state;
+//     let templateParams = {
+//       from_name: firstname,
+//       to_name: 'sansiel',
+//       reply_to : email,
+//       message_html: message,
+//       firstname,
+//       lastname,
+//       email,
+//       message
+//  }
+//  emailjs.send("gmail", "template_74tsu5x6",templateParams, "user_sWVyIEywbQTDzFH6OGZ98").then((res)=>{
+//   this.send_status.current.style.color = "green";
+//    this.setState({status: 'Ihre Nachricht wurde gesendet'})
+//    setTimeout(()=>{this.resetForm()}, 2000);
+//  }).catch();
 }
-else
-{
-  this.send_status.current.style.color = "red";
-this.setState({status: "Bitte füllen Sie das Captcha aus"});
-}   
-  }
+// else
+// {
+//   this.send_status.current.style.color = "red";
+// this.setState({status: "Bitte füllen Sie das Captcha aus"});
+// }   
+  
 render()
 {
     return(
@@ -95,13 +115,13 @@ render()
 <div className="row contactWrapper" >
   
   <div className="col-12">
-  <form  className="wpcf7" onSubmit={this.handleSubmit}>
+  <form  className="wpcf7" onSubmit = {this.handleSubmit}>
     <div className = "row">
     <div className="col-12" id= "firstinput">
-      <input required  type="text" id="fname" name="firstname" placeholder="Vorname" value={this.state.firstname} onInvalid={this.handleInvalid} onChange={this.handleChange} title="Vorname"/>
+      <input required  type="text" id="fname" name="first_name" placeholder="Vorname" value={this.state.first_name} onInvalid={this.handleInvalid} onChange={this.handleChange} title="Vorname"/>
     </div>
     <div className="col-12">
-      <input required  type="text" id="lname" name="lastname" placeholder="Nachname" title="Nachname" value={this.state.lastname} onInvalid={this.handleInvalid} onChange={this.handleChange}/>
+      <input required  type="text" id="lname" name="last_name" placeholder="Nachname" title="Nachname" value={this.state.last_name} onInvalid={this.handleInvalid} onChange={this.handleChange}/>
     </div>
     <div className="col-12 " id = "lastinput">
       <input required  type="text" id="email" name="email" placeholder="E-Mail" title="E-Mail" value={this.state.email} onInvalid={this.handleInvalid} onChange={this.handleChange} />
@@ -114,7 +134,7 @@ render()
     </div>
     </div>
     <div className="row contact_recaptcha" >
-    <ReCAPTCHA
+   {/*} <ReCAPTCHA
     className = "contact_recaptcha_main"
     ref={this.recaptchaRef}
     hl="de"
@@ -122,6 +142,7 @@ render()
     sitekey="6LeMW_8UAAAAAEviqyazs6RGElnP--XRAkJXqo0w"
     
   />
+    */}
   </div>
     <input type="submit" value="Senden" />
     <div className="status" ref={this.send_status}>{this.state.status}</div>
